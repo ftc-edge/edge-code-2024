56acc23a31442cd8da8343d4ee41b193fe41861e
thetreeeeeeeeees <seaneverestmurthy@gmail.com>
2023-12-04 18:49:03 -0500

    test stuff

--- TeamCode/src/main/java/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/teamcode/MecanumDrive.java
@@ -3,6 +3,7 @@ package teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 /**
  * This is an example minimal implementation of the mecanum drivetrain
@@ -24,21 +25,40 @@ public class MecanumDrive extends OpMode {
     private DcMotor front_right = null;
     private DcMotor back_left = null;
     private DcMotor back_right = null;
+    private DcMotor Vertical_slide = null;
+    private Servo leftClaw = null;
+    private Servo rightClaw = null;
 
     @Override
     public void init() {
 
         // Name strings must match up with the config on the Robot Controller
         // app.
+        Vertical_slide = hardwareMap.get(DcMotor.class, "VertSlide");
+
         front_left = hardwareMap.get(DcMotor.class, "topleft");
         front_right = hardwareMap.get(DcMotor.class, "topright");
         back_left = hardwareMap.get(DcMotor.class, "bottomleft");
         back_right = hardwareMap.get(DcMotor.class, "bottomright");
 
+        Vertical_slide.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        front_left.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        front_right.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        back_left.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        back_right.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        Vertical_slide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
         front_left.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         front_right.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         back_left.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         back_right.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+
+        //Vertical_slide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        Vertical_slide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
     }
 
@@ -47,9 +67,19 @@ public class MecanumDrive extends OpMode {
 
         // Mecanum drive is controlled with three axes: drive (front-and-back),
         // strafe (left-and-right), and twist (rotating the whole chassis).
+
         double drive = gamepad1.left_stick_y;
         double strafe = -gamepad1.left_stick_x;
         double twist = gamepad1.right_stick_x;
+
+        double slideSpeed = -gamepad2.left_stick_y;
+
+        if (slideSpeed > 1) {
+            slideSpeed = 1;
+        }
+        if (slideSpeed < -1) {
+            slideSpeed = -1;
+        }
 
         /*
          * If we had a gyro and wanted to do field-oriented control, here
@@ -98,9 +128,23 @@ public class MecanumDrive extends OpMode {
         }
 
         // apply the calculated values to the motors.
+
+        int position = Vertical_slide.getCurrentPosition();
+        telemetry.addData("Encoder Position", position);
+
         front_left.setPower(speeds[0]);
         front_right.setPower(speeds[1]);
         back_left.setPower(speeds[2]);
         back_right.setPower(speeds[3]);
+
+        Vertical_slide.setPower(slideSpeed);
+
+//        if (gamepad2.a) {
+//            Vertical_slide.setTargetPosition(0.5);
+//            Vertical_slide.setPower(1);
+//        }
+//        else {
+//            Vertical_slide.setPower(slideSpeed);
+//        }
     }
 }
