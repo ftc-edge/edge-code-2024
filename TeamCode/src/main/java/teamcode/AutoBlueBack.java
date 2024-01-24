f109aafe8617c62cda3bf90ee29f70c362825521
thetreeeeeeeeees <seaneverestmurthy@gmail.com>
2024-01-24 00:57:05 -0500

    auto slight changes

--- TeamCode/src/main/java/teamcode/AutoBlueBack.java
+++ TeamCode/src/main/java/teamcode/AutoBlueBack.java
@@ -230,15 +230,13 @@ public class AutoBlueBack extends LinearOpMode {
                 case SLIDE_UP:
                     // Check if the timer has exceeded the specified wait time
                     // If so, move on to the TURN_2 state
-                    if (!drive.isBusy()) {
-                        leftSlide.setTargetPosition(-1500);
-                        rightSlide.setTargetPosition(1500);
-                        leftSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                        rightSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                        leftSlide.setPower(-1);
-                        rightSlide.setPower(1);
-                        currentState = State.SWERVE_BACK;
-                    }
+                    leftSlide.setTargetPosition(-1500);
+                    rightSlide.setTargetPosition(1500);
+                    leftSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    rightSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    leftSlide.setPower(-1);
+                    rightSlide.setPower(1);
+                    currentState = State.SWERVE_BACK;
                     break;
 
                 case SWERVE_BACK:
